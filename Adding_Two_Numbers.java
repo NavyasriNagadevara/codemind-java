26ed01dec13b22bc43dfdeb98b73def2d05631d5
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-09-10 11:59:00 +0530

    import java.util.Scanner; class navya {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int k,y,d;         k=sc.nextInt();         y=sc.nextInt();         d=k+y;         System.out.println(d);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class navya
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int k,y,d;
+        k=sc.nextInt();
+        y=sc.nextInt();
+        d=k+y;
+        System.out.println(d);
+    }
+}
