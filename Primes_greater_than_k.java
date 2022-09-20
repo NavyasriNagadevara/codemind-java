281f5d4b954c26d19a176fff9a6d12393f1b945e
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-09-20 20:44:43 +0530

    import java.util.Scanner; class Navya {     public static int is_it_prime(int k)     {         int j;         for(j=2;j<=Math.sqrt(k);j++)         {             if(k%j==0)             {                 return 0;             }         }         return 1;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,c=0,k,t;         int a[]=new int[100];         n=sc.nextInt();         for(i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         k=sc.nextInt();         for(i=0;i<n;i++)         {             if(a[i]>=k)             {                 t=is_it_prime(a[i]);                 if(t==1)                 {                     c+=1;                 }             }         }         System.out.println(c);     } }

---
+++ Primes_greater_than_k.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class Navya
+{
+    public static int is_it_prime(int k)
+    {
+        int j;
+        for(j=2;j<=Math.sqrt(k);j++)
+        {
+            if(k%j==0)
+            {
+                return 0;
+            }
+        }
+        return 1;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,c=0,k,t;
+        int a[]=new int[100];
+        n=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        k=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(a[i]>=k)
+            {
+                t=is_it_prime(a[i]);
+                if(t==1)
+                {
+                    c+=1;
+                }
+            }
+        }
+        System.out.println(c);
+    }
+}
