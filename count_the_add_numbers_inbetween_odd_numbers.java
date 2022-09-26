f9666440ca3ad30e3743754c63ca20841015555a
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-09-26 15:44:44 +0530

    import java.util.Scanner; class Man{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int c=0;         int arr[]=new int[100];         for(int i=0;i<n;i++){             arr[i]=sc.nextInt();
            }
            for(int i=0;i<n-2;i++){
                if(arr[i]%2!=0 && arr[i+2]%2!=0){
                    if(arr[i+1]%2!=0){
                        c=c+1;
                    }
                }
                
            }
            System.out.println(c);
        }
    }

---
+++ count_the_add_numbers_inbetween_odd_numbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Man{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int c=0;
+        int arr[]=new int[100];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            
+        }
+        for(int i=0;i<n-2;i++){
+            if(arr[i]%2!=0 && arr[i+2]%2!=0){
+                if(arr[i+1]%2!=0){
+                    c=c+1;
+                }
+            }
+            
+        }
+        System.out.println(c);
+    }
+}
