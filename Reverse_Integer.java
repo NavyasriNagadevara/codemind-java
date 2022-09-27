de428746818b08bb21b5b96170132104c9853bab
NavyasriNagadevara <97424715+NavyasriNagadevara@users.noreply.github.com>
2022-09-27 23:41:29 +0530

    import java.util.Scanner; import java.lang.Math; class Spy {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         int d,rev=0,temp;         temp=n;         if(n<0)         {             n=Math.abs(n);         }         while(n!=0)         {             d=n%10;             rev=rev*10+d;             n=n/10;         }         if(temp<0)         {             System.out.println("-"+rev);         }         else         {             System.out.println(rev);         }     } }

---
+++ Reverse_Integer.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.lang.Math;
+class Spy
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        int d,rev=0,temp;
+        temp=n;
+        if(n<0)
+        {
+            n=Math.abs(n);
+        }
+        while(n!=0)
+        {
+            d=n%10;
+            rev=rev*10+d;
+            n=n/10;
+        }
+        if(temp<0)
+        {
+            System.out.println("-"+rev);
+        }
+        else
+        {
+            System.out.println(rev);
+        }
+    }
+}
